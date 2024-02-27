484f66758a16d27f5a4ebf9fc8ff333ee040a7a2
Дмитрий <puhov@tecon.ru>
2024-02-27 08:36:22 +0300

    #1 First commit

---
+++ BinReader.bat
@@ -0,0 +1,6 @@
+md BinRead
+copy E:\ProjectOtchet\plc.bin E:\ProjectOtchet\BinRead\plc.zip
+cd E:\ProjectOtchet\BinRead\
+tar -xvzf E:\ProjectOtchet\BinRead\plc.zip
+tar -xvzf E:\ProjectOtchet\BinRead\usual-report.tgz 
+del /S /Q E:\ProjectOtchet\BinRead\*
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿   
+   
+    namespace Idle
+    {
+        
+        public class ReadBin
+        {
+
+            public static void Main()
+            {
+
+                using (BinaryReader reader = new BinaryReader(File.Open(@"plc.bin", FileMode.Open)))
+
+                    while (reader.PeekChar() > -1)
+                        {
+                            double rp_idle = reader.ReadInt32();
+                            Console.WriteLine(rp_idle);
+                        }
+
+            }
+
+        }
+
+    }
