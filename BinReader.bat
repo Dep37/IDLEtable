22576531fbbf77a940f103881d49fbbe268716ba
Dep37 <puhov@tecon.ru>
2024-04-02 10:50:22 +0300

    #2 Добил скрипт

--- BinReader.bat
+++ BinReader.bat
@@ -1,6 +1,10 @@
 md BinRead
-copy E:\ProjectOtchet\plc.bin E:\ProjectOtchet\BinRead\plc.zip
-cd E:\ProjectOtchet\BinRead\
-tar -xvzf E:\ProjectOtchet\BinRead\plc.zip
-tar -xvzf E:\ProjectOtchet\BinRead\usual-report.tgz 
-del /S /Q E:\ProjectOtchet\BinRead\*
+copy plc.bin BinRead\plc.zip
+cd BinRead\
+tar -xvzf plc.zip
+tar -xvzf usual-report.tgz 
+cd report\
+copy *.dump ..\..\*.xls
+cd ..\
+del /S /Q *
+RMDIR /S /Q *
