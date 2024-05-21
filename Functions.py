73b7779137dedda77671460852658a2c03a34609
Dep37 <puhov@tecon.ru>
2024-05-21 12:00:21 +0300

    #18 Поправил конвертацию IDLE и добавил конвертацию  convertToTimestamp

--- Functions.py
+++ Functions.py
@@ -1,25 +1,37 @@
 import datetime
+import time
 
 # Функция конвертирования Timestamp в DataTime
 def convertToDt(millis):
     i = 0
     while i < len(millis):
         base_datetime = datetime.datetime(1970, 1, 1)
-        delta = datetime.timedelta(milliseconds=float(millis[-i])*1000)
+        delta = datetime.timedelta(milliseconds=int(float(millis[-i]))*1000)
         dt = base_datetime + delta
-        millis[-i] = "{}".format(dt)
+        millis[-i] = dt
         i += 1
-    print(millis[-1])
     return millis
-
-# Функция конвертирования DataTime в Timstamp
-
+# Функция конвертирования Timestamp в DataTime для одиночного значения
+def convertToDt1(millis):
+    base_datetime = datetime.datetime(1970, 1, 1)
+    delta = datetime.timedelta(milliseconds=int(float(millis))*1000)
+    dt = base_datetime + delta
+    millis = dt
+    return millis
+# Функция конвертирования DataTime в Timstamp, формат (2024-01-30 04:45:50)
+def convertToTimestamp(dt):
+    try:
+        datetime.datetime.fromisoformat(dt)
+        timestamp = int(time.mktime(datetime.datetime.strptime(dt, "%Y-%m-%d %H:%M:%S").timetuple()))
+    except ValueError:
+        raise ValueError("Не правильный формат даты, введите дату в формате: %Y-%m-%d %H:%M:%S")
+    return timestamp
 
 # Функция конвертирования IDLE в формат int
 def convertIDLE(idle):
     i = 0
     while i < len(idle):
-        idle[-i] = "{}".format(int(float(idle[-i])))
+        idle[-i] = int(float(idle[-i]))
         i += 1
     print(idle[-1])
     return idle
--- obj/Debug/net7.0/ProjectOtchet.AssemblyInfo.cs
+++ obj/Debug/net7.0/ProjectOtchet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectOtchet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1cc92e477d4777af961dd9f4e3f3c4358ba605e7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1361f1ad81e4ea788ec5c533e123ff05af1dac87")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectOtchet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectOtchet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
