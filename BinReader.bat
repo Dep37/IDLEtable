4a4252cdafbb2c1ff1aace25b2c3cb4f9b208b88
Зайцев Дмитрий <zaycevds@tecon.ru>
2024-05-21 07:56:30 +0300

    #15 Убрал временно вывод в консоль по батнику

--- BinReader.bat
+++ BinReader.bat
@@ -1,12 +1,15 @@
-md BinRead
-copy plc.bin BinRead\plc.zip
-cd BinRead\
-tar -xvzf plc.zip
-tar -xvzf usual-report.tgz
-cd report\
-copy *.dump ..\..\*.csv
-copy uptime ..\..\uptime.txt
-cd ..\
-del /S /Q *
-RMDIR /S /Q *
+echo off
 cls
+echo Starting BinRead.bat
+md BinRead
+copy plc.bin BinRead\plc.zip >nul 2>&1
+cd BinRead\ >nul 2>&1
+tar -xvzf plc.zip >nul 2>&1
+tar -xvzf usual-report.tgz >nul 2>&1
+cd report\ >nul 2>&1
+copy *.dump ..\..\*.csv >nul 2>&1
+copy uptime ..\..\uptime.txt >nul 2>&1
+cd ..\ >nul 2>&1
+del /S /Q * >nul 2>&1
+RMDIR /S /Q * >nul 2>&1
+echo Successful completion
